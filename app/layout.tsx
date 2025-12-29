1cbb683abec4e4108d0eaa3bb0e949b67810c2e8
ozsumit <pokhrelsumit36@gmail.com>
2025-12-29 14:24:22 +0545

    ds

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-export const dynamic = "force-dynamic";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 // @ts-ignore - side-effect global CSS import provided by Next.js
